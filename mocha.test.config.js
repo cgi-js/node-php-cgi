d24d2647a21dad59f774b39bcaca4eac491dac67
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-09-23 05:16:40 -0700

    [COMMIT] Adding minor changes to test files search

--- mocha.test.config.js
+++ mocha.test.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     spec: [
-        './test/test.*.js',
         './test/*.test.js',
+        './test/**/*.test.js',
+        './test/**/**/*.test.js',
+        './test/**/**/**/*.test.js',
+        './test/test.*.js',
         './test/**/test.*.js',
-        './test/**/*.test.js'
+        './test/**/**/test.*.js',
+        './test/**/**/**/test.*.js',
     ],
 };
